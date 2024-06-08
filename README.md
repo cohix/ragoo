40bb0f240b16862e8074b108b6a545685e08080d
cohix-lab <connor@suborbital.dev>
2024-06-08 15:35:38 -0400

    implement ollama embedder

--- README.md
+++ README.md
@@ -1,21 +1,22 @@
-# Ragoo /ræɡˈuː/
+# 🍝 Ragoo /ræɡˈuː/
+> Like the pasta dish.
 
-Like the pasta dish.
-
+## Overview
 Create Agents and RAG workflows with little to no code.
 
-Experiment with creating Agents and RAG workflows with only YAML configuration.
+- Experiment with creating Agents and RAG workflows with only YAML configuration.
+- Extend those experiments into full applications by adding tools and (optionally) writing some code.
 
-Extend experiments to become applications by adding tools and (optionally) writing some code.
+## Includes
+- Composable workflows which can be exposed as simple API endpoints or run on schedules.
+- Plugins for various services such as vector databases, LLMs, and data importers.
 
-Plugins for various services such as vector databases, LLMs, and data importers.
-
-Composable workflows which can be exposed as simple API endpoints or run on schedules.
-
+## More information
 See [the example config file](./ragoo.yaml) to get started.
 
 Requires Go 1.22 to build. Default configuration uses Ollama for easy demonstration.
 
-Apache 2.0 Licensed.
+Early experimental phase.
 
+Apache 2.0 Licensed.
 Copyright Connor Hicks and contributors, 2024.
--- pkg/embedder/ollama.go
+++ pkg/embedder/ollama.go
@@ -1,17 +1,70 @@
 package embedder
 
-import "log/slog"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"net/http"
+)
 
 type ollamaEmbedder struct {
 	config map[string]string
+}
+
+type embeddingRequest struct {
+	Model  string `json:"model"`
+	Prompt string `json:"prompt"`
+}
+
+type embeddingResponse struct {
+	Embedding []float32 `json:"embedding"`
 }
 
 // Generate generates embeddings for the given input
 func (o *ollamaEmbedder) Generate(input string) (*Result, error) {
 	slog.Info("generating embedding", "input", input)
 
+	// curl http://localhost:11434/api/embeddings -d '{
+	// 	"model": "mxbai-embed-large",
+	// 	"prompt": "Llamas are members of the camelid family"
+	//   }'
+
+	url := "http://localhost:11434/api/embeddings"
+
+	reqBody := &embeddingRequest{
+		Model:  "snowflake-arctic-embed",
+		Prompt: input,
+	}
+
+	reqBytes, err := json.Marshal(reqBody)
+	if err != nil {
+		return nil, fmt.Errorf("failed to json.Marshal: %w", err)
+	}
+
+	req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(reqBytes))
+	if err != nil {
+		return nil, fmt.Errorf("failed to NewRequest: %w", err)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to Do: %w", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("received non-200 status code: %d", resp.StatusCode)
+	}
+
+	defer resp.Body.Close()
+
+	emb := &embeddingResponse{}
+	if err := json.NewDecoder(resp.Body).Decode(emb); err != nil {
+		return nil, fmt.Errorf("failed to NewDecoder.Decode: %w", err)
+	}
+
 	r := &Result{
-		Embedding: []float32{0.1, 0.2},
+		Embedding: emb.Embedding,
 	}
 
 	return r, nil
